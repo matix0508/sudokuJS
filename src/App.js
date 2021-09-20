2d67ee3d5d223bd3cb7fd7a93371580ae3196b62
Mateusz Wlekliński <matix0508@gmail.com>
2021-09-20 13:50:50 +0200

    value clicking

--- src/App.js
+++ src/App.js
@@ -13,37 +13,44 @@ function handleKeyDown(event) {
 }
 
 function App() {
-  const [clickedSpace, setClickedSpace] = useState(null);
+  const [clickedSpace, setClickedSpace] = useState(0);
   const [board, setBoard] = useState([]);
   const [saved, setSaved] = useState(false);
-  const new_board = board;
   const grid = (
     <div class="grid grid-cols-3">
     {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((square) => (
     <Square key={square}>
       {
         [0, 1, 2, 3, 4, 5, 6, 7, 8].map((id) => {
+          let new_board = board;
           const mID = square * 9 + id;
-          if (!new_board[mID]) {
-            new_board.push({id: mID, clicked: false, changable: true, value: ''})
-          }
-          const space = new_board[mID]
-          if (space.id === clickedSpace) {
-            space.clicked = true;
-          }
+          new_board[mID] = !new_board[mID] ? {id: mID, clicked: false, changable: true, value: ''} : new_board[mID]
+          let space = new_board[mID]
+          space.clicked = space.id === clickedSpace;
+          new_board[mID] = space;
+          
           
           return (<Space 
             key={space.id}
             clicked={space.clicked}
             changable={space.changable}
+            value={space.value}
             onClick={() => {
-              board[clickedSpace].clicked = false;
+              let new_board = board
+              new_board[clickedSpace].clicked = false;
+              
               setClickedSpace(mID);
+              new_board[mID].clicked = true;
+              setBoard(new_board)
+              
             }
             }
             onKeyDown={(event) => {
               space.value = handleKeyDown(event);
-              board[space.id] = space;
+              let new_board = board
+              new_board[mID] = space
+              console.log("Space: " + new_board[mID].value)
+              setBoard(new_board)
             }
             }
           />)
@@ -53,6 +60,8 @@ function App() {
 
     </Square>
     ))}
+         
+
     
     
     
@@ -61,11 +70,16 @@ function App() {
   
   
   
+  
   return (
 
     <div className="">
     <h2 className="text-green-900 text-2xl text-center">SUDOKU SOLVER</h2>
-    <Container>
+    <Container onKeyDown={(event) => {
+      let new_board = board
+      new_board[clickedSpace].value = handleKeyDown(event);
+      setBoard(new_board);
+    }}>
       {grid}
     </Container>
     <div className="flex items-center justify-center">
--- src/Components/Space.js
+++ src/Components/Space.js
@@ -1,10 +1,4 @@
-import React, {useState} from "react";
-
-
-
 export default function Space(props) {
-    const [value, setValue] = useState("")
-    const [changable, setChangable] = useState(true);
     const colorClicked = (props.clicked && props.changable) ? ' bg-yellow-200' : '';
     const colorFixed = !props.changable ? ' bg-gray-200': '';
 
@@ -12,10 +6,10 @@ export default function Space(props) {
         <div className={""}>
             <button
                 className={"p-2 border-2 h-10 w-10 text-black" + colorClicked + colorFixed} 
-                value={value}
+                value={props.value}
                 onKeyDown={props.onKeyDown}
-                onClick={changable && props.onClick}
-                >{value}</button>
+                onClick={props.changable && props.onClick}
+                >{props.value}</button>
         </div>
     )
 }
